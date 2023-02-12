dbd9a841e79eff4514c8cdab08108fd147d2ffcd
marcocavanna <m.cavanna@proedis.net>
2023-02-12 16:06:22 +0100

    fix(utils): missing sorter type constraint

--- packages/utils/src/array/sorter/index.ts
+++ packages/utils/src/array/sorter/index.ts
@@ -1,9 +1,12 @@
+import type { GenericObject } from '@proedis/types';
+
 import ArraySorter from './ArraySorter';
+
 
 /* --------
  * Export the function to build the Sorter
  * -------- */
-export default function sorter<T extends object>(data: T[]): ArraySorter<T> {
+export default function sorter<T extends GenericObject>(data: T[]): ArraySorter<T> {
   /** Return the sorter */
   return new ArraySorter<T>(data);
 }
