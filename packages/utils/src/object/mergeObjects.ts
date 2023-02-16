5689f269478734ae81f031d53630b06d09e6d633
marcocavanna <m.cavanna@proedis.net>
2023-02-16 15:05:56 +0100

    fix(utils): ts-deepmerge work around

--- packages/utils/src/object/mergeObjects.ts
+++ packages/utils/src/object/mergeObjects.ts
@@ -4,9 +4,29 @@ import type { AnyObject } from '@proedis/types';
 
 
 /**
+ * The ts-deepmerge package seems to be broken with
+ * some build configuration (like NextJS default configuration).
+ * Workaround building a function to return valid
+ * deepmerge function (avoiding using bundler).
+ * @param m
+ */
+function getMergeDefault(m: any): typeof merge {
+  if (m && typeof m.default === 'function') {
+    return m.default;
+  }
+  else if (m && !!(m.default as any)) {
+    return getMergeDefault(m.default as any);
+  }
+
+  throw new Error('Could not get ts-deepmerge default');
+}
+
+
+/**
  * Merge two or more objects recursively, resulting in one object.
  * @param objects
  */
 export default function mergeObjects<T extends AnyObject>(...objects: Partial<T>[]): T {
-  return merge(...objects) as T;
+  // @ts-ignore
+  return getMergeDefault(merge)(...objects) as T;
 }
