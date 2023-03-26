08c74047a75e6693dd5e49ecdeb1b4c0f4630ecf
marcocavanna <m.cavanna@proedis.net>
2023-03-26 22:43:58 +0200

    feat(react-client): new hook exposed: useClientMutation

--- packages/react-client/src/context/clientWithQueryContext.tsx
+++ packages/react-client/src/context/clientWithQueryContext.tsx
@@ -6,10 +6,18 @@ import type { Client } from '@proedis/client';
 import type { ClientRequestConfig, RequestError } from '@proedis/client';
 import type { Serializable } from '@proedis/types';
 
-import { QueryClient, QueryClientProvider, replaceEqualDeep, useQuery } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider, replaceEqualDeep, useMutation, useQuery } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-import type { QueryClientConfig, QueryKey, UseQueryOptions, UseQueryResult } from '@tanstack/react-query';
+import type {
+  QueryClientConfig,
+  QueryKey,
+  UseQueryOptions,
+  UseQueryResult,
+  MutationKey,
+  UseMutationOptions,
+  UseMutationResult
+} from '@tanstack/react-query';
 
 import { createClientContext } from './clientContext';
 import type { ClientContextTools } from './clientContext';
@@ -28,6 +36,13 @@ export interface ClientWithQueryContextTools<UD extends Serializable, SD extends
     requestConfig?: Omit<ClientRequestConfig<T>, 'url'>,
     options?: Omit<UseQueryOptions<R, RequestError, R, QK>, 'queryKey' | 'queryFn'>
   ): UseQueryResult<R, RequestError>;
+
+  useClientMutation<D extends { [key: string]: any } = any, R = void, MK extends MutationKey = MutationKey>(
+    key: MK,
+    method: ClientRequestConfig<T>['method'] | ((variables: D) => ClientRequestConfig<T>['method']),
+    requestConfig?: (variables: D) => Omit<ClientRequestConfig<T>, 'url' | 'method'>,
+    options?: Omit<UseMutationOptions<R, RequestError, D, MK>, 'mutationKey' | 'mutationFn'>
+  ): UseMutationResult<R, RequestError, D, MK>;
 
 }
 
@@ -118,12 +133,40 @@ export function createClientWithQueryContext<UD extends Serializable, SD extends
 
 
   // ----
+  // Crate the useClientMutation helper
+  // ----
+  function useClientMutation<D extends { [key: string]: any } = any, R = void, MK extends MutationKey = MutationKey>(
+    key: MK,
+    method: ClientRequestConfig<T>['method'] | ((variables: D) => ClientRequestConfig<T>['method']),
+    requestConfig?: (variables: D) => Omit<ClientRequestConfig<T>, 'url' | 'method'>,
+    options?: Omit<UseMutationOptions<R, RequestError, D, MK>, 'mutationKey' | 'mutationFn'>
+  ): UseMutationResult<R, RequestError, D, MK> {
+    return useMutation<R, RequestError, D, MK>({
+      ...options,
+      mutationKey: key,
+      mutationFn : (variables) => {
+        /** Build the client request config to use with this mutation */
+        const clientRequestConfig: ClientRequestConfig<T> = {
+          data: variables,
+          ...(typeof requestConfig === 'function' ? requestConfig(variables) : {}),
+          url   : key.join('/'),
+          method: typeof method === 'function' ? method(variables) : method
+        };
+        /** Return the result of the config */
+        return client.request<R>(clientRequestConfig);
+      }
+    });
+  }
+
+
+  // ----
   // Return built context
   // ----
   return {
     ...baseContext,
     ClientWithQueryProvider,
-    useClientQuery
+    useClientQuery,
+    useClientMutation
   };
 
 }
