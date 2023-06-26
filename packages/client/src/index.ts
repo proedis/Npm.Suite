c213004364d40ce4c51aa3aed2d8288357b74d45
marcocavanna <m.cavanna@proedis.net>
2023-06-26 10:11:00 +0200

    chore(client): exporting base type of ClientSubject and Storage

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -24,6 +24,10 @@ export * from './lib/TokenHandshake/mixin/extractors';
 
 export * from './lib/TokenHandshake/mixin/transporters';
 
+export type { default as ClientSubject } from './lib/ClientSubject/ClientSubject';
+
 export type { TokenSpecification } from './lib/TokenHandshake/TokenHandshake.types';
+
+export type { default as Storage } from './lib/Storage/Storage';
 
 export type { StorageProvider, StorageApi, StoragePersistency } from './lib/Storage/Storage.types';
