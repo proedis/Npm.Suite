4971ebf9e565fd0edb40f9ce4eb9019ee58bd1e1
marcocavanna <m.cavanna@proedis.net>
2023-09-23 12:06:16 +0200

    chore(utils): strongly typed getValueAt function

--- packages/utils/src/object/getValueAt.ts
+++ packages/utils/src/object/getValueAt.ts
@@ -8,14 +8,14 @@ import type { AnyObject, ObjectPath, ValueAtPath } from '@proedis/types';
 export default function getValueAt<Values extends AnyObject, Path extends ObjectPath<Values> = ObjectPath<Values>>(
   values: Values,
   path: Path
-): ValueAtPath<Values, Path> | null {
+): ValueAtPath<Values, Path> {
 
   /** Assert object is a valid object type */
   if (!isObject(values)) {
-    return null;
+    throw new Error(`Invalid object provided to getValueAt function. Expected object, received ${typeof values}`);
   }
 
   /** Use the underlying module function */
-  return get(values, path, { default: null }) as ValueAtPath<Values, Path> | null;
+  return get(values, path, { default: null }) as ValueAtPath<Values, Path>;
 
 }
