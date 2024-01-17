f99be643ec1d427b3d25fdbfa6705b160f8805b0
marcocavanna <m.cavanna@proedis.net>
2024-01-18 00:09:19 +0100

    fix(utils): exported useful internal sorter classes

--- packages/utils/src/array/index.ts
+++ packages/utils/src/array/index.ts
@@ -1,5 +1,9 @@
 export { default as sorter } from './sorter';
 
+export { default as ArraySorter } from './sorter/ArraySorter';
+
+export { default as ArraySorterStep } from './sorter/ArraySorterStep';
+
 export type { Comparer } from './sorter/types';
 
 export * from './areArrayStrictEquals';
