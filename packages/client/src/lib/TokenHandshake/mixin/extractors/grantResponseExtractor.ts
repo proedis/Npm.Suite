aec04d5ff5e62f4d877d7ced9a27336eff129af9
marcocavanna <m.cavanna@proedis.net>
2026-04-08 19:17:39 +0200

    refactor: update grantResponseExtractor type declarations for improved type safety and consistency

--- packages/client/src/lib/TokenHandshake/mixin/extractors/grantResponseExtractor.ts
+++ packages/client/src/lib/TokenHandshake/mixin/extractors/grantResponseExtractor.ts
@@ -6,10 +6,10 @@ import type { TokenGrantResponseExtractor } from '../../TokenHandshake.types';
 export default function grantResponseExtractor<Response extends AnyObject, Tokens extends string>(
   fromGrantOf: string[],
   extractor: TokenGrantResponseExtractor<Response, Tokens>['extract']
-) {
+): TokenGrantResponseExtractor<Response, Tokens> {
   return {
-    type   : 'grant-response',
-    fromGrantOf,
-    extract: extractor
+    type       : 'grant-response',
+    fromGrantOf: fromGrantOf as TokenGrantResponseExtractor<Response, Tokens>['fromGrantOf'],
+    extract    : extractor
   };
 }
